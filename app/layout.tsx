303176aa7858dcadf0730c9efa7b31b6caebfdcd
Idriss EL Abidi <136001812+Idriss-Abidi@users.noreply.github.com>
2025-01-31 17:20:42 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+   
     <html lang="en">
+       <head>
+       {/* Tell the browser to never restore the scroll position on load */}
+  <script dangerouslySetInnerHTML={{ __html: `history.scrollRestoration = "manual"` }} />
+    </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
