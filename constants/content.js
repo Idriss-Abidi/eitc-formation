c3802d2440e34d5c5c1dc1aaa657de2bc986f265
Idriss EL Abidi <136001812+Idriss-Abidi@users.noreply.github.com>
2025-01-31 01:04:53 +0100

    Add files via upload

---
+++ constants/content.js
@@ -0,0 +1,53 @@
+// import BlogPost from "./types.tx";
+export const blogPosts = [
+  {
+    date: "November, 2024",
+    author: {
+      name: "CP Cell",
+      avatar: "/images/cp2.png",
+    },
+    title: "Introduction to Competitive Programming",
+    pdf: "/pdfs/cp0_Introduction.pdf",
+    image: "/covers/cp0.png",
+  },
+  {
+    date: "November, 2024",
+    author: {
+      name: "CP Cell",
+      avatar: "/images/cp2.png",
+    },
+    title: "Getting Started",
+    pdf: "/pdfs/cp1_GettingStarted.pdf",
+    image: "/covers/cp1.png",
+  },
+  {
+    date: "December, 2024",
+    author: {
+      name: "CP Cell",
+      avatar: "/images/cp2.png",
+    },
+    title: "Introduction to Data Structures",
+    pdf: "/pdfs/cp2_DataStructures.pdf",
+    image: "/covers/cp2.png",
+  },
+  {
+    date: "January, 2025",
+    author: {
+      name: "CP Cell",
+      avatar: "/images/cp2.png",
+    },
+    title: "Sorting & Binary Search",
+    pdf: "/pdfs/cp3_Sorting.pdf",
+    image: "/covers/cp3.png",
+  },
+  {
+    date: "January, 2025",
+    author: {
+      name: "CP Cell",
+      avatar: "/images/cp2.png",
+    },
+    title: "Complete search",
+    pdf: "/pdfs/cp4_CompleteSearch.pdf",
+    image: "/covers/cp4.png",
+  },
+];
